4d9a85ea8d71538d03f9fd5773e541c951ac9b03
guifersv <gui.fer.sv@gmail.com>
2025-08-24 12:15:04 -0300

    Fixes

--- astronvim/nvim/init.lua
+++ astronvim/nvim/init.lua
@@ -23,37 +23,6 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
-local rzls_path = vim.fn.expand("$MASON/packages/rzls/libexec")
-vim.lsp.config("roslyn", {
-    cmd = {
-        "roslyn",
-        "--stdio",
-        "--logLevel=Information",
-        "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-        "--razorSourceGenerator=" .. vim.fs.joinpath(rzls_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
-        "--razorDesignTimePath=" .. vim.fs.joinpath(rzls_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
-        "--extension",
-        vim.fs.joinpath(rzls_path, "RazorExtension", "Microsoft.VisualStudioCode.RazorExtension.dll"),
-    },
-    settings = {
-        ["csharp|inlay_hints"] = {
-            csharp_enable_inlay_hints_for_implicit_object_creation = true,
-            csharp_enable_inlay_hints_for_implicit_variable_types = true,
-            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-        },
-        ["csharp|code_lens"] = {
-            dotnet_enable_references_code_lens = true,
-        },
-        ["csharp|formatting"] = {
-            dotnet_organize_imports_on_format = true,
-        },
-        ["csharp|completion"] = {
-            dotnet_show_completion_items_from_unimported_namespaces = true,
-            dotnet_show_name_completion_suggestions = true,
-        },
-    },
-})
-
 require "lazy_setup"
 require "polish"
 require "mason-config"
