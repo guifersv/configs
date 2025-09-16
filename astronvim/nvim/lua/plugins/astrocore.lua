fb4cc11fe4be24a481aca2cee08a3b7b8c4b4488
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-09-16 11:57:31 -0300

    Update Kanagawa theme

--- astronvim/nvim/lua/plugins/astrocore.lua
+++ astronvim/nvim/lua/plugins/astrocore.lua
@@ -1,3 +1,4 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
@@ -57,8 +58,7 @@ return {
       -- first key is the mode
       n = {
         -- second key is the lefthand side of the map
-        ["x"] = {'"_x'},
-        ["c"] = {'"_c'},
+
         -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
@@ -79,10 +79,6 @@ return {
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
-      },
-      v = {
-        ["x"] = {'"_x'},
-        ["c"] = {'"_c'},
       },
     },
   },
---
+++ astronvim/nvim/lua/plugins/ui.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      picker = {
+        -- your picker configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+    },
+  },
+  {
+    "AstroNvim/astroui",
+    ---@type AstroUIOpts
+    opts = {
+      highlights = {
+        -- set highlights for all themes
+        -- use a function override to let us use lua to retrieve
+        -- colors from highlight group there is no default table
+        -- so we don't need to put a parameter for this function
+        init = function()
+          local get_hlgroup = require("astroui").get_hlgroup
+          -- get highlights from highlight groups
+          local bg = get_hlgroup("Normal").bg
+          local bg_alt = get_hlgroup("Visual").bg
+          local green = get_hlgroup("String").fg
+          local red = get_hlgroup("Error").fg
+          -- return a table of highlights for snacks.picker based on
+          -- colors retrieved from highlight groups
+          return {
+            SnacksPickerBorder = { fg = bg_alt, bg = bg },
+            SnacksPicker = { bg = bg },
+            SnacksPickerPreviewBorder = { fg = bg, bg = bg },
+            SnacksPickerPreview = { bg = bg },
+            SnacksPickerPreviewTitle = { fg = bg, bg = green },
+            SnacksPickerBoxBorder = { fg = bg, bg = bg },
+            SnacksPickerInputBorder = { fg = bg, bg = bg },
+            SnacksPickerInputSearch = { fg = red, bg = bg },
+            SnacksPickerListBorder = { fg = bg, bg = bg },
+            SnacksPickerList = { bg = bg },
+            SnacksPickerListTitle = { fg = bg, bg = bg },
+          }
+        end,
+      },
+    },
+  },
+
+  require("kanagawa").setup {
+    colors = {
+      theme = {
+        dragon = {
+          ui = {
+            bg_gutter = "#181616",
+          },
+        },
+      },
+    },
+  },
+}
