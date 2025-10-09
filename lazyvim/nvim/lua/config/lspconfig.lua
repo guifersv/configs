f5512bcee07166903515ec63760e6040317cb05e
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-09 15:10:16 -0300

    Update lazyvim

--- lazyvim/nvim/lua/config/lspconfig.lua
+++ lazyvim/nvim/lua/config/lspconfig.lua
@@ -1,1 +1,17 @@
-vim.lsp.config("roslyn", {})
+vim.lsp.config("roslyn", {}) -- no vim.lsp.enable() necessary here
+
+-- IMPORTANT: vim diagnostic configuration AFTER LSPs are loaded
+vim.diagnostic.config({
+  underline = false,
+  virtual_text = false,
+  update_in_insert = false,
+  severity_sort = true,
+  signs = {
+    text = {
+      [vim.diagnostic.severity.ERROR] = " ",
+      [vim.diagnostic.severity.WARN] = " ",
+      [vim.diagnostic.severity.HINT] = " ",
+      [vim.diagnostic.severity.INFO] = " ",
+    },
+  },
+})
