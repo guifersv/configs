f1bc1dc58ca766d3bf90f86f8780cf674f00f0b2
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-17 15:09:21 -0300

    Auto open nvim tree

--- nvchad/nvim/lua/autocmds.lua
+++ nvchad/nvim/lua/autocmds.lua
@@ -1,10 +1,17 @@
 require "nvchad.autocmds"
 
-vim.api.nvim_create_autocmd("VimEnter", {
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("NvimTree_start_directory", { clear = true }),
+  desc = "Start Nvim-tree with directory",
+  once = true,
   callback = function()
-    local arg = vim.fn.argv(0)
-    if arg == "." or vim.fn.isdirectory(arg) == 1 then
-      require("nvim-tree.api").tree.open()
+    if package.loaded["nvim-tree"] then
+      return
+    else
+      local stats = vim.uv.fs_stat(vim.fn.argv(0))
+      if stats and stats.type == "directory" then
+        require("nvim-tree.api").tree.open()
+      end
     end
   end,
 })
