6ea128cad9afdbf086c3611670cfa703eee0ec89
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-29 14:51:55 -0300

    Not log on roslyn attach; Change file explorer to neo-tree

--- lazyvim/nvim/lua/config/lazy.lua
+++ lazyvim/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.editor.neo-tree" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
 
     -- import/override with your plugins
