bab6e269893b1f488cd36124ee5b2a4e5e76cf4d
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-11-27 21:15:28 -0300

    Some Fixes on lazy setup

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,11 +1,4 @@
 return {
   "folke/noice.nvim",
-  opts = {
-    views = {
-      notify = { enabled = false },
-    },
-    lsp = {
-      progress = { enabled = false },
-    },
-  },
+  enabled = false,
 }
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,19 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "toml",
+      "vim",
+      "vimdoc",
+      "xml",
+      "yaml",
+      "c_sharp",
+    },
+  },
+}
